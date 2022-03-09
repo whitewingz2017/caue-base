2ace0f8005fd1465430f967b8d07ca14f0d57f49
Cauê <45627677+CaueDev@users.noreply.github.com>
2022-03-09 13:30:11 -0300

    clothes

---
+++ resources/[assets]/[clothes]/caue-assets-clothes/fxmanifest.lua
@@ -0,0 +1,29 @@
+fx_version "cerulean"
+games { "gta5" }
+
+files {
+    "mp_m_freemode_01_mp_m_trvie_m.meta",
+    "mp_m_freemode_01_mp_m_trvie_june.meta",
+    "mp_f_freemode_01_mp_f_trvie_f.meta",
+    "mp_m_freemode_01_mp_m_gz_hat.meta",
+    "mp_f_freemode_01_mp_f_gz_hat.meta",
+    "mp_m_freemode_01_mp_m_anz_m.meta",
+    "mp_m_freemode_01_mp_m_anxiety_team.meta",
+    "mp_f_freemode_01_mp_f_anz_f.meta",
+    "mp_m_freemode_01_mp_m_limited.meta",
+    "mp_f_freemode_01_grzybeek_clothes_shop.meta",
+    "first_person_alternates.meta",
+    "peds.meta",
+}
+
+data_file "SHOP_PED_APPAREL_META_FILE" "mp_m_freemode_01_mp_m_trvie_m.meta"
+data_file "SHOP_PED_APPAREL_META_FILE" "mp_m_freemode_01_mp_m_trvie_june.meta"
+data_file "SHOP_PED_APPAREL_META_FILE" "mp_f_freemode_01_mp_f_trvie_f.meta"
+data_file "SHOP_PED_APPAREL_META_FILE" "mp_m_freemode_01_mp_m_gz_hat.meta"
+data_file "SHOP_PED_APPAREL_META_FILE" "mp_f_freemode_01_mp_f_gz_hat.meta"
+data_file "SHOP_PED_APPAREL_META_FILE" "mp_m_freemode_01_mp_m_anz_m.meta"
+data_file "SHOP_PED_APPAREL_META_FILE" "mp_m_freemode_01_mp_m_anxiety_team.meta"
+data_file "SHOP_PED_APPAREL_META_FILE" "mp_f_freemode_01_mp_f_anz_f.meta"
+data_file "SHOP_PED_APPAREL_META_FILE" "mp_m_freemode_01_mp_m_limited.meta"
+data_file "SHOP_PED_APPAREL_META_FILE" "mp_f_freemode_01_grzybeek_clothes_shop.meta"
+data_file "PED_FIRST_PERSON_ALTERNATE_DATA" "first_person_alternates.meta"
