cc1fda11ddba2e1e8e96fb9dc2d4bd982372841d
nhedber1 <43317214+nhedber1@users.noreply.github.com>
2022-08-02 01:02:00 +0000

    fixed some sql stuff
    fixed some sql stuff

--- database.sql
+++ database.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE  IF NOT EXISTS `caue-rp` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `caue-rp`;
+CREATE DATABASE  IF NOT EXISTS `database-name` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `database-name`;
 -- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
 --
 -- Host: localhost    Database: database-name
